1bc6b2b48572cad79095b353d155c08973bb64d0
ayushverma7011 <ayushverma7011@gmail.com>
2025-03-28 15:45:26 +0530

    Exploring GET, PUT, POST API

--- src/app.js
+++ src/app.js
@@ -2,8 +2,37 @@ const express = require('express');
 
 const app= express();
 
-app.use((req, res) =>{
-    res.send("Namaste from the Dashboard! I am Admin of the server");
+// app.use("/", (req, res) =>{
+//     res.send("Namaste from the Dashboard! I am Admin of the server");
+// });
+// app.use("/hello/2", (req, res) => {
+//     res.send("ndbb jbccbdswcbcbcvbd");
+// });
+// app.use("/hello", (req, res) => {
+//     res.send("Hello hello hello");
+// });
+
+app.get("/user", (req, res) => {
+    res.send("firstname: Ayush,  lastname: Verma ");
+});
+
+app.post("/user", (req, res) => {
+    res.send("Data has been added successfully");
+});
+app.delete("/user", (req, res) => {
+    res.send("Data has been deleted successfully");
+});
+
+app.patch("/user", (req, res) => {
+    res.send("Data has been updated successfully");
+});
+
+app.put("/user", (req, res) => {
+    res.send("Data has been replaced successfully");
+});
+
+app.head("/user", (req, res) => {
+    res.send("Data has been checked successfully");
 });
 
 app.listen(7777, () =>{
